53ec756c4b303b01b9df6368082ac1f2430dba7d
VeeSimz <kayveesimba@gmail.com>
2024-09-18 02:52:53 +0200

    Fix documentation errors

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
-	const msg = process.stdin.read();
-	if (msg !== null) {
-		process.stdout.write(`Your name is: ${data.toString()}`);
-		process.exit();
-	}
+  const msg = process.stdin.read();
+  if (msg !== null) {
+    process.stdout.write(`Your name is: ${msg}`);
+    process.exit();
+  }
 });
 
 process.stdin.on('end', () => {
-	  process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
