e29db6d53e5f4c2def95563830adab3440c35b53
VeeSimz <kayveesimba@gmail.com>
2024-09-18 03:11:16 +0200

    Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('readable', () => {
   const msg = process.stdin.read();
   if (msg !== null) {
     process.stdout.write(`Your name is: ${msg}`);
-    process.exit();
-  }
+    }
 });
 
 process.stdin.on('end', () => {
